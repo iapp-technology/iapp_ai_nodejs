716a80d93e62de85367d7d773dfda39f18e9a6ad
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2023-01-20 15:58:12 +0700

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,5 +1,5 @@
 const iapp_ai = require("iapp_ai")
-const api = new iapp_ai("EF4cHwGyV2WhwEUtD4BO33VBQpyvZKGG");
+const api = new iapp_ai("{Your API Key}");
 
 async function test(){
     const ret = await api.kaitom_tts("สวัสดี");
