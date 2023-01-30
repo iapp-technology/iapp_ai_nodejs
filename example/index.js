33d8ea5f32b6e669d78d170ce1002af55b479eb8
Sreyroth Phem (Lord) <71324265+srothhh@users.noreply.github.com>
2023-01-30 10:13:45 +0700

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,8 +1,14 @@
 const iapp_ai = require("iapp_ai")
 const api = new iapp_ai("{Your API Key}");
 
-async function test(){
+async function test_tts(){
     const ret = await api.kaitom_tts("สวัสดี");
     console.log(ret)
 }
 test()
+
+async function test_idcardFront_Ocr(){
+    const ret = await api.idcardFront_Ocr("media/id-card-front.jpg");
+    console.log(ret)
+}
+test_idcardFront_Ocr()
